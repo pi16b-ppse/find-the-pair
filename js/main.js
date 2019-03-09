3217738cb91c4f60b39ce0f1133ffb2b416a6004
VladaMamutova <36194644+VladaMamutova@users.noreply.github.com>
2019-03-09 16:12:00 +0300

    style: add couple of missing gaps
    Changed formatting in order to stick with the 1TBS style.

--- js/main.js
+++ js/main.js
@@ -7,35 +7,35 @@ const cardGrid = document.getElementById("card-grid");
 
 // Генерирует карточки для игровой сетки.
 function generateCard(number){
-  let card = document.createElement('div');
-  card.classList.add("card-wrapper");
-  card.innerHTML = '<div class="card"><img class="front" src="'+images[number]+'" alt="'+number+'"></div></div>';
-  return card;
+    let card = document.createElement('div');
+    card.classList.add("card-wrapper");
+    card.innerHTML = '<div class="card"><img class="front" src="'+images[number]+'" alt="'+number+'"></div></div>';
+    return card;
 }
 
 // Создаёт пары карточек и добавляет их на игровую сетку.
 function initCards(){
-  // Создаём массив последовательно идущих попарных картинок.
-  let initialCards = [];
-  for(let i = 0;i<numberOfCards/2;i++){
-    initialCards.push(generateCard(i));
-    initialCards.push(generateCard(i));
-  }
-
-  // Перемешиваем картинки.
-  let cards = [];
-  while(initialCards.length>0){
-    let ranNum = Math.floor(Math.random()*initialCards.length);
-    cards.push(initialCards[ranNum]);
-    if(ranNum>-1){
-      initialCards.splice(ranNum,1);
+// Создаём массив последовательно идущих попарных картинок.
+    let initialCards = [];
+    for(let i = 0;i<numberOfCards/2;i++){
+        initialCards.push(generateCard(i));
+        initialCards.push(generateCard(i));
     }
-  }
 
-  // Добавляем карточки на игровую сетку.
-  for(let i = 0;i<cards.length;i++){
-    cardGrid.appendChild(cards[i]);
-  }
+    // Перемешиваем картинки.
+    let cards = [];
+    while(initialCards.length>0){
+        let ranNum = Math.floor(Math.random()*initialCards.length);
+        cards.push(initialCards[ranNum]);
+        if(ranNum>-1){
+            initialCards.splice(ranNum,1);
+        }
+    }
+
+    // Добавляем карточки на игровую сетку.
+    for(let i = 0;i<cards.length;i++){
+        cardGrid.appendChild(cards[i]);
+    }
 }
 
 initCards();
